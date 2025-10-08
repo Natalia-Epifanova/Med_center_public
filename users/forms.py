c072a297405d66f9100f059ea1698fba882a3221
Natalia Epifanova <epif97@mail.ru>
2025-10-08 11:55:32 +0300

    add authenticate (login/logout) and edit user profile in app

---
+++ users/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+
+from users.models import User
+
+
+class UserRegisterForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ("email", "password1", "password2")
+
+
+class UserProfileForm(ModelForm):
+
+    class Meta:
+        model = User
+        fields = ("email",)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView
 
-# Create your views here.
+from users.forms import UserProfileForm, UserRegisterForm
+from users.models import User
+
+
+class CustomLoginView(LoginView):
+    template_name = "users/login.html"
+
+
+class UserCreateView(CreateView):
+
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy("users:login")
+
+
+class UserUpdateView(UpdateView):
+
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy("timetable:home")
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = "users/profile_detail.html"
