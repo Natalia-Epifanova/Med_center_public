ddbe8190e8183d3166c1482c33cda9a53d2ba116
Natalia Epifanova <epif97@mail.ru>
2026-01-21 18:10:53 +0300

    add CRUD for doctor treatment

--- timetable/context_processors.py
+++ timetable/context_processors.py
@@ -8,6 +8,7 @@ def user_permissions(request):
         "is_medical_admin": request.user.groups.filter(
             name="Medical Center Administrator"
         ).exists(),
+        "is_doctor": request.user.groups.filter(name="Doctors").exists(),
         "can_manage_schedule": request.user.groups.filter(name="Admin").exists(),
         "can_create_emergency_slots": request.user.groups.filter(
             name="Medical Center Administrator"
